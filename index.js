f1a95702bb7514ccdf487d6429addf21e7b4fc83
Ryan Carlos <ryancarlos925@gmail.com>
2022-04-07 12:11:39 -0700

    commit tree 010437a07f52b6850ff7b978d5e1394db6245892

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(totalFeet){
+  let result
+  if (totalFeet <= 400) {
+    result = "This one is on me!";
+    return result
+  }
+  else if (totalFeet > 400 && totalFeet < 2000) {
+    result = 'That will be twenty bucks.';
+    return result
+  }
+  else if (totalFeet > 2000 && totalFeet < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+    return result
+  }
+  else 
+    result = 'No can do.';
+    return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let isCity;
+  city === "NYC" ? (isCity = 'Ok, sounds good.') : (isCity = "No go.");
+  return isCity
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let charm;
+  switch (tip) {
+    case 'generous':
+      charm = 'Thank you so much.';
+      break;
+      case 'not as generous':
+        charm = 'Thank you.';
+        break;
+        default :
+        charm = "Bye.";
+  }
+  return charm
 }
